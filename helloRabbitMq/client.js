cfdab9a5148dfbdd18083ae3d3fac85331d78ffd
OctoberCity <1730653658@qq.com>
2018-11-19 10:52:46 +0800

    helloworld rabbitMQ

--- helloRabbitMq/client.js
+++ helloRabbitMq/client.js
@@ -1,15 +1,28 @@
-// 生产者
-const amqplib = require("amqplib");
-const openCon = amqplib.connect("amqp://127.0.0.1");
-const taskname = "helloworld";
+// // 生产者
+// const amqplib = require("amqplib");
+// const openCon = amqplib.connect("amqp://127.0.0.1");
+// const taskname = "helloworld";
 
-openCon
-.then((connection)=>{
-	return connection.createChannel();
-})
-.then((channel)=>{
-return ch.assertQueue(taskname).then((ok)=>{
-     return ch.sendToQueue(taskname,Buffer.from('hello world'))
+// openCon
+// .then((connection)=>{
+// 	return connection.createChannel();
+// })
+// .then((channel)=>{
+// return ch.assertQueue(taskname).then((ok)=>{
+//      return ch.sendToQueue(taskname,Buffer.from('hello world'))
+//   });
+// })
+// .catch(console.warn);
+
+var q = 'tasks';
+
+var open = require('amqplib').connect('amqp://localhost');
+
+// Publisher
+open.then(function(conn) {
+  return conn.createChannel();
+}).then(function(ch) {
+  return ch.assertQueue(q).then(function(ok) {
+    return ch.sendToQueue(q, Buffer.from('something to do'));
   });
-})
-.catch(console.warn);
+}).catch(console.warn);
