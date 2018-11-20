caf0fb0943e153ce3dcba28974e93466755bdfed
OctoberCity <1730653658@qq.com>
2018-11-20 13:17:46 +0800

    路由模式

---
+++ routerRabbit/client.js
@@ -0,0 +1,29 @@
+// 生产者  fanout是广播
+const amqplib = require("amqplib/callback_api");
+const taskname = "helloCallBack";
+
+//获取信息和路由参数(只带有一个路由参数和一个message的情况下)
+const args =process.argv.slice(2);
+const mes =args.slice(1)[0];
+const routerPar = args.[0];
+
+
+
+function error(err,channel){
+ 	console.error(err);
+ 	if(channel)process.exit(1);
+ }
+
+function listConnect(err,conn){
+   	if(err) return  error(err);
+  conn.createChannel((err,channel)=>{
+    // 使用direct而不是fanout扇出
+   	channel.assertExchange(taskname,'direct',{durable:fasle});
+ 
+   	channel.publish(taskname,routerPar,Buffer.from(msg));
+   	console.log("发送完毕，关闭 发送消息是：==》",mes);
+   	channel.close(()=>{conn.close()});
+     });
+   };
+
+    amqplib.connect("amqp://127.0.0.1",listConnect);
---
+++ routerRabbit/server.js
@@ -0,0 +1,39 @@
+//  消费者
+const amqplib = require("amqplib/callback_api");
+const taskname = "helloCallBack";
+
+//获取路由参数
+const routerPar =process.argv.slice(2);  
+
+
+
+function(err,channel){
+	console.error(err);
+	if(channel){process.exit(1);}
+}
+
+function listConnection(err,connection){
+  if(err) return error(err);
+  connection.createChannel((err,channel)=>{
+  	if(err) return error(err,channel);
+  	channel.assertExchange(taskname,'direct',{durable:false});
+  	channel.assertQueue('',{exclusive:true},(err,ok)=>{
+  		if(err) return error(err);
+  		const queue=ok.queue;
+         routerPar.forEach((item)=>{
+         	//绑定对列路由
+         	channel.bindQueue(queue,taskname,item);
+         });
+         // 消费对列中特定路由消息
+         channel.consume(queue,(msg)=>{
+         	console.log("路由是",msg.fields.routerKey);
+         	console.log("内容是"，msg.content);
+         },{noAck:true});
+  	});
+  });
+}
+
+
+amqplib.connect('amqp:127.0.0.1',listConnection);
+
+
