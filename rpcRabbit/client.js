624a5b83013f4e78a7b20b3366e1e1b285e1f77a
OctoberCity <1730653658@qq.com>
2018-11-22 09:50:15 +0800

    修改rpc通信bugger

--- rpcRabbit/client.js
+++ rpcRabbit/client.js
@@ -1,9 +1,7 @@
 const amqplib = require("amqplib/callback_api");
 const taskname = "RPC";
 
-amqplib.connect("amqp://127.0.0.1",listConnect);
-
-const num =parseInt(process.argv[3]); 
+const num =parseInt(process.argv[2]); 
 const id = (new Date()).getTime();
 
 function error(err,channel){
@@ -15,20 +13,22 @@ function listConnect(err,connection){
 	if(err)return error(err);
 	connection.createChannel((err,channel)=>{
 		if(err)return(err,channel);
-		channel.assert('',{exclusive:true},(err,ok)=>{
-			if(err)return channel(channel);
+		channel.assertQueue('',{exclusive:true},(err,ok)=>{
+			if(err)return channel(err,channel);
 			const queue = ok.queue;
 			//消费服务端发过来的消息
-			channel.consume(queue,(messsage)=>{
-				if(messsage,properties.correlationId=== id){
-				console.log("得到的结果是",messsage.content.toString());
+			channel.consume(queue,(message)=>{
+				if(message.properties.correlationId== id){
+				console.log("得到的结果是",message.content.toString());
 			}else{
 				console.log("我山大王"+id+"还没接到消息啊。。。。。。");
 				channel.close(()=>{connection.close();});
 			}
 			},{noAck:true});
 			//发送给服务端
-			channel.sendToQueue(taskname,Buffer.from(num+''),correlationId:id,replyTo:queue);
+			console.log(num);
+			channel.sendToQueue(taskname,Buffer.from(num+''),{correlationId:id+'',replyTo:queue});
 		});
 	});
 }
+amqplib.connect("amqp://127.0.0.1",listConnect);
