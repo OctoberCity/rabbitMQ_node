3d0ac59900da643fef8d9f9853988c8293ae8dcc
OctoberCity <1730653658@qq.com>
2018-12-07 15:04:52 +0800

    修改格式

--- RMQVCallback/client.js
+++ RMQVCallback/client.js
@@ -2,28 +2,28 @@
 const amqplib = require("amqplib/callback_api");
 const taskname = "helloCallBack";
 
- amqplib.connect("amqp://127.0.0.1",listConnect);
+amqplib.connect("amqp://127.0.0.1", listConnect);
 
 
-function listConnect(err,connection){
-    if(err) return error(err);
-    connection.createChannel((err,channel)=>{
-        if(err)return error(err,channel);
-    	channel.assertQueue(taskname);
-    		// 取得控制台输入的参数
-    		const params = process.argv.slice(2);
-    	        console.log(params);	
-               channel.sendToQueue(taskname,Buffer.from(params.join('')));;
+function listConnect(err, connection) {
+    if (err) return error(err);
+    connection.createChannel((err, channel) => {
+        if (err) return error(err, channel);
+        channel.assertQueue(taskname);
+        // 取得控制台输入的参数
+        const params = process.argv.slice(2);
+        console.log(params);
+        channel.sendToQueue(taskname, Buffer.from(params.join('')));;
     });
 
 
 }
- 
- function error(err,channel){
- 	if(err)console.log(err.message);
- 	if(channel){
-                console.log("出错了");
- 		channel.close();
- 		prcess.exit(1);
- 	}
- }
+
+function error(err, channel) {
+    if (err) console.log(err.message);
+    if (channel) {
+        console.log("出错了");
+        channel.close();
+        prcess.exit(1);
+    }
+}
--- RMQVCallback/server.js
+++ RMQVCallback/server.js
@@ -2,19 +2,19 @@
 const amqplib = require("amqplib/callback_api");
 const taskname = "helloCallBack";
 
- amqplib.connect("amqp://127.0.0.1",listConnect);
+amqplib.connect("amqp://127.0.0.1", listConnect);
 
 
-function listConnect(err,connection){
-    connection.createChannel((err,channel)=>{
-    	channel.assertQueue(taskname);
-    	channel.consume(taskname,(message)=>{
-            console.log("接收到的数据是",message.content.toString()); 
-             setTimeout(()=>{
-                console.log("三秒插入数据库");   
+function listConnect(err, connection) {
+    connection.createChannel((err, channel) => {
+        channel.assertQueue(taskname);
+        channel.consume(taskname, (message) => {
+            console.log("接收到的数据是", message.content.toString());
+            setTimeout(() => {
+                console.log("三秒插入数据库");
                 channel.ack(message);
-               },3000);
-           });
+            }, 3000);
+        });
     });
 
 
